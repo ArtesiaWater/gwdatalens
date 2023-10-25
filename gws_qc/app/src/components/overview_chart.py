165b7562c683ada0f0d2d0d25be9eff21eefce9c
Davíd Brakenhoff <d.brakenhoff@artesia-water.nl>
2023-10-25 11:11:31 +0200

    add callback to plot time series add plot series function

--- gws_qc/app/src/components/overview_chart.py
+++ gws_qc/app/src/components/overview_chart.py
@@ -3,11 +3,37 @@ from functools import partial
 import dash_bootstrap_components as dbc
 from dash import Dash, Input, Output, State, dcc, html
 from dash.exceptions import PreventUpdate
+import plotly.graph_objs as go
+import pandas as pd
 
 from . import ids
+from ..data.source import DataSource
 
 
-def render(app: Dash):
+def render(app: Dash, data: DataSource):
+    @app.callback(
+        Output(ids.SERIES_CHART, "figure"),
+        [Input(ids.OVERVIEW_MAP, "selectedData")],
+        # prevent_initial_call=True,
+        allow_duplicate=True,
+    )
+    def plot_series(selectedData):
+        # print("point=", selectedData)
+
+        if selectedData is not None:
+            
+            pts = pd.DataFrame(selectedData["points"])
+
+            # get selected points
+            if not pts.empty:
+                names = pts["text"].tolist()
+            else:
+                names = None
+            return plot_obs(names, data)
+        else:
+            # print("no update")
+            return {"layout": {"title": "No series selected."}}
+
     return html.Div(
         id="series-chart-div",
         children=[
@@ -38,3 +64,49 @@ def render(app: Dash):
             "margin-bottom": 10,
         },
     )
+
+
+def plot_obs(names, data):
+    
+    hasobs = [i for i, _ in data.list_locations()]
+
+    if names is None:
+        return {"layout": {"title": "No series to plot"}}
+
+    traces = []
+    for name in names:
+        # no obs
+        if name not in hasobs:
+            print("no data", name)
+            return {"layout": {"title": "No series to plot"}}
+        
+        ts = data.get_timeseries(gmw_id=name, tube_id=1, column="field_value")
+        print("time series", name)
+        if ts is None:
+            continue
+        trace_i = go.Scattergl(
+            x=ts.index,
+            y=ts.values,
+            mode="markers",
+            marker={"color": "blue", "size": 3},
+            name=name,
+            legendgroup="0",
+            showlegend=True,
+        )
+        traces.append(trace_i)
+    layout = {
+        # "xaxis": {"range": [sim.index[0], sim.index[-1]]},
+        "yaxis": {"title": "(m NAP)"},
+        "legend": {
+            "traceorder": "reversed+grouped",
+            "orientation": "h",
+            "xanchor": "left",
+            "yanchor": "bottom",
+            "x": 0.0,
+            "y": 1.02,
+        },
+        "dragmode": "pan",
+        # "margin": dict(t=70, b=40, l=40, r=10),
+    }
+
+    return dict(data=traces, layout=layout)
