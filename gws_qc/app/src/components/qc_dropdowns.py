67c30bdc6e92d0e95212c573bf8255d1885e5da1
Davíd Brakenhoff <d.brakenhoff@artesia-water.nl>
2023-11-23 23:38:37 +0100

    use selected map state to feed into dropdown

--- gws_qc/app/src/components/qc_dropdowns.py
+++ gws_qc/app/src/components/qc_dropdowns.py
@@ -1,18 +1,25 @@
-from dash import html, dcc, Dash
+from typing import List, Optional
+from dash import html, dcc
 
 from . import ids
 from ..data.source import DataSource
 
 
-def render_selection_series_dropdown(data: DataSource):
+def render_selection_series_dropdown(data: DataSource, selected_data: Optional[List]):
     locs = data.list_locations()
     locs = [f"{iloc[0]}-{iloc[1]:03g}" for iloc in locs]
     options = [{"label": i, "value": i} for i in locs]
+
+    if selected_data is not None and len(selected_data) == 1:
+        value = selected_data[0]
+    else:
+        value = None
 
     return html.Div(
         children=[
             dcc.Dropdown(
                 options=options,
+                value=value,
                 clearable=True,
                 searchable=True,
                 placeholder="Select time series ...",
