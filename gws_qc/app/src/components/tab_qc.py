c8238fcf28c089980b12882bba2f677f0cacbaa5
Davíd Brakenhoff <d.brakenhoff@artesia-water.nl>
2023-11-23 17:39:56 +0100

    add qc page dropdowns

--- gws_qc/app/src/components/tab_qc.py
+++ gws_qc/app/src/components/tab_qc.py
@@ -4,11 +4,11 @@ import dash_bootstrap_components as dbc
 from dash import Dash, Input, Output, State, dcc, html
 from dash.exceptions import PreventUpdate
 
-from . import ids, qc_chart, qc_rules, qc_table
+from . import ids, qc_chart, qc_dropdowns, qc_rules, qc_table, qc_traval_button
 from ..data.source import DataSource
 
 
-def render(app: Dash):
+def render():
     return dcc.Tab(
         label="QC",
         value=ids.TAB_QC,
@@ -17,19 +17,35 @@ def render(app: Dash):
     )
 
 
-def render_content(app: Dash, data: DataSource):
-    print("haha")
+def render_content(data: DataSource):
     return dbc.Container(
         [
             dbc.Row(
                 [
-                    dbc.Col([qc_chart.render(app, data)], width=12),
+                    dbc.Col(
+                        [qc_dropdowns.render_selection_series_dropdown(data)],
+                        width=4,
+                    ),
+                    dbc.Col(
+                        [qc_dropdowns.render_additional_series_dropdown(data)],
+                        width=4,
+                    ),
                 ]
             ),
             dbc.Row(
                 [
-                    dbc.Col([qc_rules.render(app, data)], width=6),
-                    dbc.Col([qc_table.render(app, data)], width=6),
+                    dbc.Col([qc_chart.render()], width=12),
+                ]
+            ),
+            dbc.Row(
+                [
+                    dbc.Col([qc_rules.render()], width=6),
+                    dbc.Col([qc_table.render()], width=6),
+                ]
+            ),
+            dbc.Row(
+                [
+                    dbc.Col([qc_traval_button.render()], width=6),
                 ]
             ),
         ],
