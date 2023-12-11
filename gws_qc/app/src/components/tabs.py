94991c2aebf6a7472fa51405545bfe4b217db994
Davíd Brakenhoff <d.brakenhoff@artesia-water.nl>
2023-12-11 15:52:32 +0100

    add tabs to tab container component

--- gws_qc/app/src/components/tabs.py
+++ gws_qc/app/src/components/tabs.py
@@ -1,14 +1,16 @@
 from dash import dcc
 
-from . import ids, tab_overview, tab_qc
+from . import ids, tab_overview, tab_qc, tab_model, tab_traval
 
 
 def render():
     return dcc.Tabs(
         id=ids.TAB_CONTAINER,
-        value=ids.TAB_QC,
+        value=ids.TAB_TRAVAL,
         children=[
             tab_overview.render(),
             tab_qc.render(),
+            tab_model.render(),
+            tab_traval.render(),
         ],
     )
