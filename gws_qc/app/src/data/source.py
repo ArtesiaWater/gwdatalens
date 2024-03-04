3989b66ba4379e99181bf33b08b8e260132badfb
Ruben Caljé <r.calje@artesia-water.nl>
2024-03-04 17:36:13 +0100

    Edit DataSource to use sqlalchemy and add rule_explenation_nl

--- gws_qc/app/src/data/source.py
+++ gws_qc/app/src/data/source.py
@@ -1,15 +1,15 @@
 import logging
-import os
-import pickle
 from functools import cached_property, lru_cache
-from typing import List, Tuple
+from typing import List
 
 import geopandas as gpd
 import numpy as np
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objs as go
-import sqlalchemy as sa
+from sqlalchemy import create_engine, select, ForeignKey, DateTime, func, update
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+from datetime import datetime
 import traval
 from icecream import ic
 from pyproj import Transformer
@@ -246,14 +246,120 @@ class TravalInterface:
         return dict(data=traces, layout=layout)
 
 
+class Base(DeclarativeBase):
+    pass
+
+
+class GroundwaterMonitoringWell(Base):
+    __tablename__ = "groundwater_monitoring_wells"
+
+    groundwater_monitoring_well_id: Mapped[int] = mapped_column(primary_key=True)
+    bro_id: Mapped[str]
+    nitg_code: Mapped[str]
+    tubes: Mapped[List["GroundwaterMonitoringTubesStatic"]] = relationship(
+        back_populates="groundwater_monitoring_well", cascade="all, delete-orphan"
+    )
+
+    # groundwater_monitoring_tube_static_id: Mapped[int]
+
+
+class GroundwaterMonitoringTubesStatic(Base):
+    __tablename__ = "groundwater_monitoring_tubes_static"
+    groundwater_monitoring_tube_static_id: Mapped[int] = mapped_column(primary_key=True)
+    screen_length: Mapped[float]
+    tube_number: Mapped[int]
+    groundwater_monitoring_well_id: Mapped[int] = mapped_column(
+        ForeignKey("groundwater_monitoring_wells.groundwater_monitoring_well_id")
+    )
+    groundwater_monitoring_well: Mapped["GroundwaterMonitoringWell"] = relationship(
+        back_populates="tubes"
+    )
+    dynamic: Mapped[List["GroundwaterMonitoringTubesDynamic"]] = relationship(
+        back_populates="groundwater_monitoring_tubes_static",
+        cascade="all, delete-orphan",
+    )
+
+
+class GroundwaterMonitoringTubesDynamic(Base):
+    __tablename__ = "groundwater_monitoring_tubes_dynamic"
+    groundwater_monitoring_tube_dynamic_id: Mapped[int] = mapped_column(
+        primary_key=True
+    )
+    tube_top_position: Mapped[float]
+    plain_tube_part_length: Mapped[float]
+    groundwater_monitoring_tube_static_id: Mapped[int] = mapped_column(
+        ForeignKey(
+            "groundwater_monitoring_tubes_static.groundwater_monitoring_tube_static_id"
+        )
+    )
+    groundwater_monitoring_tubes_static: Mapped["GroundwaterMonitoringTubesStatic"] = (
+        relationship(back_populates="dynamic")
+    )
+
+
+class DeliveredLocations(Base):
+    __tablename__ = "delivered_locations"
+    location_id: Mapped[int] = mapped_column(primary_key=True)
+    coordinates: Mapped[str]
+    referencesystem: Mapped[str]
+    groundwater_monitoring_well_id: Mapped[int] = mapped_column(
+        ForeignKey("groundwater_monitoring_wells.groundwater_monitoring_well_id")
+    )
+
+
+class GroundwaterLevelDossier(Base):
+    __tablename__ = "groundwater_level_dossier"
+    groundwater_level_dossier_id: Mapped[int] = mapped_column(primary_key=True)
+    gmw_bro_id: Mapped[str]
+    tube_number: Mapped[int]
+
+
+class Observation(Base):
+    __tablename__ = "observation"
+    observation_id: Mapped[int] = mapped_column(primary_key=True)
+    groundwater_level_dossier_id: Mapped[int] = mapped_column(
+        ForeignKey("groundwater_level_dossier.groundwater_level_dossier_id")
+    )
+    observation_metadata_id: Mapped[int] = mapped_column(
+        ForeignKey("observation_metadata.observation_metadata_id")
+    )
+
+
+class ObservationMetadata(Base):
+    __tablename__ = "observation_metadata"
+    observation_metadata_id: Mapped[int] = mapped_column(primary_key=True)
+    observation_type: Mapped[str]
+
+
+class MeasurementTvp(Base):
+    __tablename__ = "measurement_tvp"
+    measurement_tvp_id: Mapped[int] = mapped_column(primary_key=True)
+    measurement_time: Mapped[datetime] = mapped_column(DateTime(timezone=True))
+    field_value: Mapped[float]
+    field_value_unit: Mapped[str]
+    observation_id: Mapped[int] = mapped_column(
+        ForeignKey("observation.observation_id")
+    )
+    measurement_point_metadata_id: Mapped[int] = mapped_column(
+        ForeignKey("measurement_point_metadata.measurement_point_metadata_id")
+    )
+
+
+class MeasurementPointMetadata(Base):
+    __tablename__ = "measurement_point_metadata"
+    measurement_point_metadata_id: Mapped[int] = mapped_column(primary_key=True)
+    qualifier_by_category: Mapped[str]
+
+
 class DataSource:
     def __init__(self):
         # init connection to database OR just read in some data from somewhere
         # Connect to database using psycopg2
         try:
-            self.engine = sa.create_engine(
+            self.engine = create_engine(
                 f"postgresql+psycopg2://{config.user}:{config.password}@"
-                f"{config.host}:{config.port}/{config.database}"
+                f"{config.host}:{config.port}/{config.database}",
+                connect_args={"options": "-csearch_path=gmw,gld"},
             )
 
             logger.info("Database connected successfully")
@@ -262,7 +368,7 @@ class DataSource:
             logger.error("Database not connected successfully")
 
         self.value_column = "field_value"
-        self.qualifier_column = "qualifier"
+        self.qualifier_column = "qualifier_by_category"
         self.source = "zeeland"
 
     @cached_property
@@ -283,50 +389,29 @@ class DataSource:
                 lat
                 lon
         """
-
-        # get a DataFrame with the properties of all wells
-        wells = self._get_table_df("gmw.groundwater_monitoring_wells")
-
-        # get a GeoDataFrame with locations
-        table_name = "gmw.delivered_locations"
-        query = sa.text(f"select *  FROM {table_name}")
-        with self.engine.connect() as connection:
-            locs = gpd.GeoDataFrame.from_postgis(
-                query, connection, geom_col="coordinates"
+        stmt = (
+            select(
+                GroundwaterMonitoringWell.bro_id,
+                GroundwaterMonitoringWell.nitg_code,
+                GroundwaterMonitoringTubesStatic.tube_number,
+                GroundwaterMonitoringTubesDynamic.tube_top_position,
+                GroundwaterMonitoringTubesDynamic.plain_tube_part_length,
+                GroundwaterMonitoringTubesStatic.screen_length,
+                DeliveredLocations.coordinates,
+                DeliveredLocations.referencesystem,
             )
+            .join(GroundwaterMonitoringTubesStatic.groundwater_monitoring_well)
+            .join(GroundwaterMonitoringTubesDynamic)
+            .join(DeliveredLocations)
+        )
+        # tubes = pd.read_sql(stmt, con=engine)
+        gdf = gpd.GeoDataFrame.from_postgis(stmt, self.engine, geom_col="coordinates")
+        # for duplicates only keep the last combination of bro_id and tube_number
+        gdf = gdf[~gdf.duplicated(subset=["bro_id", "tube_number"], keep="last")]
+
         # make sure all locations are in EPSG:28992
         msg = "Other coordinate reference systems than RD not supported yet"
-        assert (locs["referencesystem"].str.lower() == "rd").all, msg
-        # drop duplicate location entries (TODO: find out why they are there)
-        mask = locs["groundwater_monitoring_well_id"].duplicated()
-        if mask.any():
-            logger.info(f"Dropping {mask.sum()} duplicate delivered_locations")
-            locs = locs[~mask]
-
-        # add information from locations wells
-        wells = wells.merge(locs, how="left", on="groundwater_monitoring_well_id")
-
-        # combine static and dynamic tube-info
-        tube_dyn = self._get_table_df("gmw.groundwater_monitoring_tubes_dynamic")
-        tube_dyn = tube_dyn[
-            ~tube_dyn["groundwater_monitoring_tube_static_id"].duplicated(keep="last")
-        ]
-
-        tubes = self._get_table_df("gmw.groundwater_monitoring_tubes_static")
-        tubes = tubes.merge(
-            tube_dyn, how="left", on="groundwater_monitoring_tube_static_id"
-        )
-
-        # only keep wells with a gmw
-        tubes = tubes[
-            tubes["groundwater_monitoring_well_id"].isin(
-                wells["groundwater_monitoring_well_id"]
-            )
-        ]
-        # add information from wells to tubes
-        tubes = tubes.merge(wells, how="left", on="groundwater_monitoring_well_id")
-
-        gdf = gpd.GeoDataFrame(tubes, geometry="coordinates")
+        assert (gdf["referencesystem"].str.lower() == "rd").all, msg
 
         # calculate top filter and bottom filter
         gdf["screen_top"] = gdf["tube_top_position"] - gdf["plain_tube_part_length"]
@@ -337,8 +422,7 @@ class DataSource:
         )
 
         # set bro_id and tube_number as index
-        gdf = gdf.set_index(["bro_id", "tube_number"])
-        gdf = gdf.reset_index().set_index("name")
+        gdf = gdf.set_index("name")
 
         # add number of measurements
         gdf["metingen"] = 0
@@ -367,7 +451,7 @@ class DataSource:
         """Return a list of locations that contain groundwater level dossiers, where
         each location is defines by a tuple of length 2: bro-id and tube_id"""
         # get all grundwater level dossiers
-        df = self._get_table_df("gld.groundwater_level_dossier")
+        df = pd.read_sql(select(GroundwaterLevelDossier), con=self.engine)
         # get unique combinations of gmw id and tube id
         loc_df = df[["gmw_bro_id", "tube_number"]].drop_duplicates()
         locations = [
@@ -398,62 +482,40 @@ class DataSource:
     def get_timeseries(self, gmw_id: str, tube_id: int) -> pd.Series:
         """Return a Pandas Series for the measurements at the requested bro-id and
         tube-id, im m. Return None when there are no measurements."""
-        # get groundwater_level_dossier_id
-        table_name = "gld.groundwater_level_dossier"
-        query = f"select groundwater_level_dossier_id FROM {table_name} WHERE gmw_bro_id = '{gmw_id}' AND tube_number = {tube_id}"
-        cursor = self._execute_query(query)
-        gld_ids = [x[0] for x in cursor.fetchall()]
-        if len(gld_ids) == 0:
-            logger.info(f"No data found for {gmw_id}, {tube_id}")
-            return None
-
-        # get observation_id
-        table_name = "gld.observation"
-        gld_ids_str = str(gld_ids).replace("[", "").replace("]", "")
-        query = f"select observation_id FROM {table_name} WHERE groundwater_level_dossier_id in ({gld_ids_str})"
-        cursor = self._execute_query(query)
-        observation_ids = [x[0] for x in cursor.fetchall()]
-        if len(observation_ids) == 0:
-            logger.info(f"No data found for {gmw_id}, {tube_id}")
-            return None
-
-        # get measurements from table gld.measurement_tvp
-        table_name = "gld.measurement_tvp"
-        observation_ids_str = str(observation_ids).replace("[", "").replace("]", "")
-        query = f"select * FROM {table_name} WHERE observation_id in ({observation_ids_str})"
-        mtvp = self._query_to_df(query).set_index("measurement_time")
+        stmt = (
+            select(
+                MeasurementTvp.measurement_time,
+                MeasurementTvp.field_value,
+                MeasurementPointMetadata.qualifier_by_category,
+                MeasurementTvp.field_value_unit,
+                MeasurementTvp.measurement_tvp_id,
+                MeasurementTvp.measurement_point_metadata_id,
+            )
+            .join(MeasurementPointMetadata)
+            .join(Observation)
+            .join(ObservationMetadata)
+            .join(GroundwaterLevelDossier)
+            .filter(
+                GroundwaterLevelDossier.gmw_bro_id.in_([gmw_id]),
+                GroundwaterLevelDossier.tube_number.in_([tube_id]),
+                ObservationMetadata.observation_type == "reguliereMeting",
+            )
+            .order_by(MeasurementTvp.measurement_time)
+        )
+        df = pd.read_sql(stmt, con=self.engine, index_col="measurement_time")
 
         # make sure all measurements are in m
-        mask = mtvp["field_value_unit"] == "cm"
+        mask = df["field_value_unit"] == "cm"
         if mask.any():
-            mtvp.loc[mask, self.value_column] /= 100
-            mtvp.loc[mask, "field_value_unit"] = "m"
+            df.loc[mask, self.value_column] /= 100
+            df.loc[mask, "field_value_unit"] = "m"
 
         # convert all other measurements to NaN
-        mask = mtvp["field_value_unit"] != "m"
+        mask = df["field_value_unit"] != "m"
         if mask.any():
-            mtvp.loc[mask, self.value_column] = np.nan
+            df.loc[mask, self.value_column] = np.nan
         # msg = "Other units than m or cm not supported yet"
         # assert (mtvp["field_value_unit"] == "m").all(), msg
-
-        mtvp[self.value_column] = pd.to_numeric(mtvp[self.value_column])
-
-        # get measurement_point_metadata_id
-        table_name = "gld.measurement_point_metadata"
-        mpm_id = mtvp["measurement_point_metadata_id"]
-        measurement_point_metadata_ids_str = (
-            str(list(mpm_id.unique())).replace("[", "").replace("]", "")
-        )
-        query = f"select * FROM {table_name} WHERE measurement_point_metadata_id in ({measurement_point_metadata_ids_str})"
-        mpm = self._query_to_df(query).set_index("measurement_point_metadata_id")
-
-        # add qualifier to time-value-pairs
-        mtvp[self.qualifier_column] = mpm.loc[
-            mtvp["measurement_point_metadata_id"], "qualifier_by_category"
-        ].values
-
-        # only keep value and qualifier, and sort index
-        df = mtvp[[self.value_column, self.qualifier_column]].sort_index()
 
         # make index DateTimeIndex
         if df.index.dtype == "O":
@@ -465,44 +527,32 @@ class DataSource:
 
         return df
 
-    # def set_qualifier(self, df, qualifier="goedgekeurd"):
-    #    table_name = "gld.type_status_quality_control"
-    #    query = f"select qualifier_by_category_id FROM {table_name} WHERE value = '{qualifier}'"
-    #    cursor = self._execute_query(query)
-    #    gld_ids = [x[0] for x in cursor.fetchall()]
-    #    if len(gld_ids) == 0:
-    #        raise (Exception(f"Unknown qualifier: {qualifier}"))
-
-    def _get_all_tables(self):
-        cursor = self._execute_query("SELECT table_name FROM information_schema.tables")
-        tables = [x for x in cursor.fetchall()]
-        # tables = sa.inspect(self.engine).get_table_names()
-        return tables
-
-    def _get_tables_in_schema(self, schema):
-        cursor = self._execute_query(
-            f"SELECT table_name FROM information_schema.tables WHERE table_schema = '{schema}'"
+    def count_measurements_per_filter(self):
+        stmt = (
+            select(
+                GroundwaterLevelDossier.gmw_bro_id,
+                GroundwaterLevelDossier.tube_number,
+                func.count(MeasurementTvp.measurement_tvp_id).label("Metingen"),
+            )
+            .join(MeasurementPointMetadata)
+            .join(Observation)
+            .join(ObservationMetadata)
+            .join(GroundwaterLevelDossier)
+            .group_by(
+                GroundwaterLevelDossier.gmw_bro_id, GroundwaterLevelDossier.tube_number
+            )
+            .filter(ObservationMetadata.observation_type == "reguliereMeting")
         )
-        tables = [x[0] for x in cursor.fetchall()]
-        return tables
+        count = pd.read_sql(stmt, con=self.engine)
+        return count
 
-    def _exists_table(self, table):
-        cursor = self._execute_query(
-            f"select exists(select * FROM information_schema.tables where table_name={table})"
+    def set_qualifier(self, df, qualifier="goedgekeurd"):
+        mask = df["qualifier_by_category"] != qualifier
+        ids = list(df.loc[mask, "measurement_point_metadata_id"])
+        stmt = (
+            update(MeasurementPointMetadata)
+            .where(MeasurementPointMetadata.measurement_point_metadata_id.in_(ids))
+            .values(qualifier_by_category="goedgekeurd")
         )
-        return cursor.fetchone()[0]
-        # return self.engine.dialect.has_table(self.engine.connect(), table)
-
-    def _get_table_df(self, table_name):
-        query = f"select * FROM {table_name}"
-        return self._query_to_df(query)
-
-    def _execute_query(self, query):
-        with self.engine.connect() as connection:
-            result = connection.execute(sa.text(query))
-        return result
-
-    def _query_to_df(self, query):
-        with self.engine.connect() as connection:
-            df = pd.read_sql_query(sa.text(query), con=connection)
-        return df
+        with self.engine.begin() as conn:
+            conn.execute(stmt)
