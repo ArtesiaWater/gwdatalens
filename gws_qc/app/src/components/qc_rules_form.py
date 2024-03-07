a869576edc32a0e17e42500745582ac89eef6b9a
Davíd Brakenhoff <d.brakenhoff@artesia-water.nl>
2024-03-08 00:10:07 +0100

    use ruleset for series

--- gws_qc/app/src/components/qc_rules_form.py
+++ gws_qc/app/src/components/qc_rules_form.py
@@ -2,12 +2,11 @@ from inspect import signature
 
 import dash_bootstrap_components as dbc
 import numpy as np
-from dash import dash_table, html
-from dash.dash_table.Format import Format
+from dash import html
+
 
 from ..data.source import DataInterface
 from . import ids
-from .styling import DATA_TABLE_HEADER_BGCOLOR
 
 
 def generate_kwargs_from_func(func):
@@ -161,86 +160,16 @@ def generate_traval_rule_components(rule, rule_number, series_name=None):
     return irow
 
 
-def render_rules_table(data):
-    rule_table = data.traval.to_dataframe().loc[:, ["name", "apply_to", "kwargs"]]
-    rule_table = rule_table.reset_index().astype(str)
-
-    return html.Div(
-        id="qc-rules-div",
-        children=[
-            dash_table.DataTable(
-                id=ids.QC_RULES_TABLE,
-                data=rule_table.to_dict("records"),
-                columns=[
-                    {
-                        "id": "step",
-                        "name": "Step",
-                        "type": "numeric",
-                        "format": Format(scheme="r", precision=1),
-                        "editable": False,
-                    },
-                    {
-                        "id": "name",
-                        "name": "Rule",
-                        "type": "text",
-                        "editable": False,
-                    },
-                    {
-                        "id": "apply_to",
-                        "name": "Apply to",
-                        "type": "numeric",
-                        "format": Format(scheme="r", precision=1),
-                        "editable": True,
-                    },
-                    #     {
-                    #         "id": "kwargs",
-                    #         "name": "Parameters",
-                    #         "type": "text",
-                    #         "editable": True,
-                    #     },
-                ],
-                editable=False,
-                fixed_rows={"headers": True},
-                page_action="none",
-                # style_table={
-                #     # "height": "70vh",
-                #     # "overflowY": "auto",
-                #     # "margin-top": 15,
-                #     # "maxHeight": "70vh",
-                # },
-                row_selectable="multi",
-                style_cell={"whiteSpace": "pre-line", "fontSize": 12},
-                style_cell_conditional=[
-                    {"if": {"column_id": "step"}, "width": "20%"},
-                    {"if": {"column_id": "name"}, "width": "40%"},
-                    {"if": {"column_id": "apply_to"}, "width": "40%"},
-                    # {"if": {"column_id": "kwargs"}, "width": "75%"},
-                ]
-                + [
-                    {
-                        "if": {"column_id": c},
-                        "textAlign": "left",
-                    }
-                    for c in ["name", "kwargs"]
-                ],
-                # style_data_conditional=style_data_conditional,
-                style_header={
-                    "backgroundColor": DATA_TABLE_HEADER_BGCOLOR,
-                    "fontWeight": "bold",
-                },
-            ),
-        ],
-        className="dbc dbc-row-selectable",
-    )
-
-
 def render_traval_form(data: DataInterface, series_name=None):
     form_components = []
-    nrules = len(data.traval.ruleset.rules) - 1
+    nrules = len(data.traval._ruleset.rules) - 1
+
+    # reset ruleset to original version
+    # data.traval._ruleset = deepcopy(data.traval.ruleset)
 
     idx = 0
     for i in range(1, nrules + 1):
-        irule = data.traval.ruleset.get_rule(istep=i)
+        irule = data.traval._ruleset.get_rule(istep=i)
         irow = generate_traval_rule_components(irule, idx, series_name=series_name)
         form_components.append(irow)
         idx += 1
