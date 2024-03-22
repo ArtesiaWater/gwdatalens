d417d9cc4a2a7723dfea0b395d24a147a6de31fe
Davíd Brakenhoff <d.brakenhoff@artesia-water.nl>
2024-03-22 08:58:26 +0100

    add datepicker tmin/tmax to model tab

--- gws_qc/app/callbacks/models.py
+++ gws_qc/app/callbacks/models.py
@@ -2,6 +2,7 @@ import json
 import os
 
 import i18n
+import pandas as pd
 import pastas as ps
 from dash import Input, Output, State, no_update
 from dash.exceptions import PreventUpdate
@@ -30,15 +31,26 @@ def register_model_callbacks(app, data):
         Output(ids.ALERT_BODY, "children", allow_duplicate=True),
         Input(ids.MODEL_GENERATE_BUTTON, "n_clicks"),
         State(ids.MODEL_DROPDOWN_SELECTION, "value"),
+        State(ids.MODEL_DATEPICKER_TMIN, "date"),
+        State(ids.MODEL_DATEPICKER_TMAX, "date"),
         prevent_initial_call=True,
     )
-    def generate_model(n_clicks, value):
+    def generate_model(n_clicks, value, tmin, tmax):
         if n_clicks is not None:
             if value is not None:
                 try:
+                    tmin = pd.Timestamp(tmin)
+                    tmax = pd.Timestamp(tmax)
                     ml = data.pstore.create_model(value, add_recharge=True)
-                    ml.solve(freq="D", report=False, noise=False)
-                    ml.solve(freq="D", noise=True, report=False, initial=False)
+                    ml.solve(freq="D", tmin=tmin, tmax=tmax, report=False, noise=False)
+                    ml.solve(
+                        freq="D",
+                        tmin=tmin,
+                        tmax=tmax,
+                        noise=True,
+                        report=False,
+                        initial=False,
+                    )
                     mljson = json.dumps(
                         ml.to_dict(), cls=PastasEncoder
                     )  # store generated model
@@ -105,6 +117,8 @@ def register_model_callbacks(app, data):
         Output(ids.ALERT, "is_open", allow_duplicate=True),
         Output(ids.ALERT, "color", allow_duplicate=True),
         Output(ids.ALERT_BODY, "children", allow_duplicate=True),
+        Output(ids.MODEL_DATEPICKER_TMIN, "date"),
+        Output(ids.MODEL_DATEPICKER_TMAX, "date"),
         Input(ids.MODEL_DROPDOWN_SELECTION, "value"),
         prevent_initial_call=True,
     )
@@ -119,6 +133,8 @@ def register_model_callbacks(app, data):
                     True,  # show alert
                     "success",  # alert color
                     f"Loaded time series model '{value}' from PastaStore.",  # empty alert message
+                    ml.settings["tmin"].to_pydatetime(),
+                    ml.settings["tmax"].to_pydatetime(),
                 )
             except Exception as _:
                 return (
@@ -128,6 +144,8 @@ def register_model_callbacks(app, data):
                     True,  # show alert
                     "warning",  # alert color
                     f"No model available for {value}. Click 'Generate Model' to create one.",
+                    None,
+                    None,
                 )
         elif value is None:
             return (
@@ -137,4 +155,6 @@ def register_model_callbacks(app, data):
                 False,  # show alert
                 "success",  # alert color
                 "",  # empty message
+                None,
+                None,
             )
