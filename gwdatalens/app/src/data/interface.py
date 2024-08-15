478c5e5b3bd557e05e152488b0b3f97d2ef2439d
Davíd Brakenhoff <d.brakenhoff@artesia-water.nl>
2024-08-15 11:47:04 +0200

    add updating of stresses in pastastore

--- gwdatalens/app/src/data/interface.py
+++ gwdatalens/app/src/data/interface.py
@@ -1,11 +1,20 @@
 class DataInterface:
-    def __init__(self, db=None, pstore=None, traval=None):
+    def __init__(self, db=None, pstore=None, traval=None, **kwargs):
         self.db = db
-        self.pstore = pstore
-        self.traval = traval
 
-    def attach_pastastore(self, pstore):
+        self.pstore = self.attach_pastastore(
+            pstore, update_knmi=kwargs.get("update_knmi", False)
+        )
+
+        self.attach_traval(traval)
+
+    def attach_pastastore(self, pstore, update_knmi=False):
         self.pstore = pstore
+        if update_knmi:
+            from pastastore.extensions import activate_hydropandas_extension
+
+            activate_hydropandas_extension()
+            self.pstore.hpd.update_knmi_meteo()
 
     def attach_traval(self, traval):
         self.traval = traval
