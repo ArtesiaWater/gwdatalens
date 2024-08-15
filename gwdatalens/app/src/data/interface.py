89fc5d1fcba4e13ee997e49aadafaa917172fd85
Davíd Brakenhoff <d.brakenhoff@artesia-water.nl>
2024-08-15 17:06:47 +0200

    pass tmax to pstore update

--- gwdatalens/app/src/data/interface.py
+++ gwdatalens/app/src/data/interface.py
@@ -13,13 +13,15 @@ class DataInterface:
 
     def attach_pastastore(self, pstore, update_knmi=False):
         self.pstore = pstore
+
         if update_knmi:
             from pastastore.extensions import activate_hydropandas_extension
 
             activate_hydropandas_extension()
             # set tmax to 4 weeks ago
             tmax = pd.Timestamp.today().normalize() - pd.Timedelta(days=7 * 4)
-            self.pstore.hpd.update_knmi_meteo(tmax=tmax)
+            pstore.hpd.update_knmi_meteo(tmax=tmax)
+        return pstore
 
     def attach_traval(self, traval):
         self.traval = traval
