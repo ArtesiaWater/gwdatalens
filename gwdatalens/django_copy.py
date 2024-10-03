bce528bb649bde415c99ebb9882eae61be73a83c
Davíd Brakenhoff <d.brakenhoff@artesia-water.nl>
2024-10-03 11:32:44 +0200

    improve django copy - modify template path - add docstring - set logging level

--- gwdatalens/django_copy.py
+++ gwdatalens/django_copy.py
@@ -9,6 +9,35 @@ logger = logging.getLogger(__name__)
 def copy_gwdatalens_to_django_app(
     DJANGO_APP_PATH, DATALENS_PATH=pl.Path(__file__).parent
 ):
+    """Copies the GWDataLens application to a specified Django application path.
+
+    Parameters
+    ----------
+    DJANGO_APP_PATH : str or pathlib.Path
+        The path to the Django application where the GWDataLens app and assets will be
+        copied.
+    DATALENS_PATH : pathlib.Path, optional
+        The path to the GWDataLens source directory. Defaults to the parent directory
+        of the current file.
+
+    Notes
+    -----
+    - Copies the GWDataLens app to the `gwdatalens` folder within the specified Django
+      app path.
+    - Copies assets to the `static/dash` folder within the specified Django app path.
+    - Copies Django-specific files to the `gwdatalens` folder within the specified
+      Django app path.
+    - Copies the Dash template to the `templates` folder within the specified Django
+      app path.
+
+    Raises
+    ------
+    shutil.Error
+        If an error occurs during the copying process.
+    """
+    if isinstance(DJANGO_APP_PATH, str):
+        DJANGO_APP_PATH = pl.Path(DJANGO_APP_PATH)
+
     logger.info("Copying GWDataLens to Django App...")
     # copy app to gwdatalens folder
     shutil.copytree(
@@ -49,15 +78,19 @@ def copy_gwdatalens_to_django_app(
     logger.info(" - copying dash template")
     shutil.copy(
         DATALENS_PATH / "django" / "templates" / "dash.html",
-        DJANGO_APP_PATH / "main" / "templates",
+        DJANGO_APP_PATH / "templates",
     )
-    logger.info(f"Done! Copied GWDataLens to Django project in {DJANGO_APP_PATH}.")
+    logger.info(f"Done! Copied GWDataLens to Django project in '{DJANGO_APP_PATH}/'.")
 
 
 # %%
 if __name__ == "__main__":
+    logging.basicConfig()
     logger.setLevel(logging.INFO)
+    # david
     DJANGO_APP_PATH = pl.Path("/home/david/github/bro-connector/bro_connector")
+    # provincie zeeland
+    # DJANGO_APP_PATH = pl.Path("C:/BRO/bro-provincie-zeeland/bro_connector")
     copy_gwdatalens_to_django_app(DJANGO_APP_PATH)
 
 # %%
