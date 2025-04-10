33f8c690c5ba8bcc5fd69759de4d8c1c91d58f4d
Davíd Brakenhoff <d.brakenhoff@artesia-water.nl>
2025-04-10 15:08:13 +0200

    add file finders to plotly_settings

--- gwdatalens/django/plotly_settings.py
+++ gwdatalens/django/plotly_settings.py
@@ -28,6 +28,8 @@ PLOTLY_DASH = {
 }
 
 STATICFILES_FINDERS = [
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
     "django_plotly_dash.finders.DashAssetFinder",
     "django_plotly_dash.finders.DashComponentFinder",
     "django_plotly_dash.finders.DashAppDirectoryFinder",
