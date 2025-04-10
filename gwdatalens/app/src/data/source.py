7d5f1a9a9002f4af4ba7a4d677e84410092e96ee
Davíd Brakenhoff <d.brakenhoff@artesia-water.nl>
2025-04-10 14:27:48 +0200

    convert to float

--- gwdatalens/app/src/data/source.py
+++ gwdatalens/app/src/data/source.py
@@ -450,7 +450,7 @@ class PostgreSQLDataSource(DataSourceTemplate):
             # make sure all measurements are in m
             mask = df["field_value_unit"] == "cm"
             if mask.any():
-                df.loc[mask, "field_value"] /= 100
+                df.loc[mask, "field_value"] /= 100.0
                 df.loc[mask, "field_value_unit"] = "m"
 
             # convert all other measurements to NaN
