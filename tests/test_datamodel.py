29b4524b082e5cb1c4603b6530135c1853245f33
Davíd Brakenhoff <d.brakenhoff@artesia-water.nl>
2025-05-19 12:06:25 +0200

    add tests for sqlachemy datamodel objects

---
+++ tests/test_datamodel.py
@@ -0,0 +1,70 @@
+# Tests for datamodel.py SQLAlchemy models
+import pytest
+from sqlalchemy.orm import Session
+
+from gwdatalens.app.settings import config
+from gwdatalens.app.src.data import PostgreSQLDataSource
+from gwdatalens.app.src.data.datamodel import (
+    GroundwaterLevelDossier,
+    MeasurementPointMetadata,
+    MeasurementTvp,
+    Observation,
+    ObservationMetadata,
+    TubeDynamic,
+    TubeStatic,
+    Well,
+)
+
+
+@pytest.fixture
+def engine():
+    # postgreql database
+    return PostgreSQLDataSource(config=config["database"]).engine
+
+
+def test_query_well(engine):
+    with Session(engine) as session:
+        wells = session.query(Well).limit(5).all()
+        assert isinstance(wells, list)
+
+
+def test_query_tube_static(engine):
+    with Session(engine) as session:
+        tubes = session.query(TubeStatic).limit(5).all()
+        assert isinstance(tubes, list)
+
+
+def test_query_tube_dynamic(engine):
+    with Session(engine) as session:
+        tubes = session.query(TubeDynamic).limit(5).all()
+        assert isinstance(tubes, list)
+
+
+def test_query_groundwater_level_dossier(engine):
+    with Session(engine) as session:
+        dossiers = session.query(GroundwaterLevelDossier).limit(5).all()
+        assert isinstance(dossiers, list)
+
+
+def test_query_observation(engine):
+    with Session(engine) as session:
+        obs = session.query(Observation).limit(5).all()
+        assert isinstance(obs, list)
+
+
+def test_query_observation_metadata(engine):
+    with Session(engine) as session:
+        meta = session.query(ObservationMetadata).limit(5).all()
+        assert isinstance(meta, list)
+
+
+def test_query_measurement_tvp(engine):
+    with Session(engine) as session:
+        tvps = session.query(MeasurementTvp).limit(5).all()
+        assert isinstance(tvps, list)
+
+
+def test_query_measurement_point_metadata(engine):
+    with Session(engine) as session:
+        meta = session.query(MeasurementPointMetadata).limit(5).all()
+        assert isinstance(meta, list)
