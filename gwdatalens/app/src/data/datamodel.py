84152dd7b09c8ba8b2fd1c0fea0cc99d3aa724d8
dbrakenhoff <d.brakenhoff@artesia-water.nl>
2025-11-05 13:28:32 +0100

    ruff

--- gwdatalens/app/src/data/datamodel.py
+++ gwdatalens/app/src/data/datamodel.py
@@ -17,15 +17,18 @@ class Well(Base):
     coordinates: Mapped[str]
     reference_system: Mapped[str]
 
+
 class WellDynamic(Base):
     __tablename__ = "groundwater_monitoring_well_dynamic"
-    ground_water_monitoring_well_dynamic_id: Mapped[int] = mapped_column(primary_key=True)
+    ground_water_monitoring_well_dynamic_id: Mapped[int] = mapped_column(
+        primary_key=True
+    )
     ground_level_position: Mapped[float]
     groundwater_monitoring_well_static_id: Mapped[int] = mapped_column(
         ForeignKey(
             "groundwater_monitoring_well_static.groundwater_monitoring_well_static_id"
         )
-    )   
+    )
 
 
 class TubeStatic(Base):
