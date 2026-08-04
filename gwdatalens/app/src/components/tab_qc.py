94d9efd9ce5f3d7c05cf8e5f4ede38c371b224cd
dbrakenhoff <d.brakenhoff@artesia-water.nl>
2026-08-04 21:44:53 +0200

    simplify qc callbacks to add reset QC option

--- gwdatalens/app/src/components/tab_qc.py
+++ gwdatalens/app/src/components/tab_qc.py
@@ -193,6 +193,7 @@ def render_content(data: DataManager, selected_data: List[int]) -> dbc.Container
                         ),
                         width="auto",
                     ),
+                    dbc.Col([qc_traval_buttons.render_reset_qc_button()], width="auto"),
                     dbc.Col(
                         [qc_traval_buttons.render_run_traval_button()], width="auto"
                     ),
@@ -265,10 +266,7 @@ def render_content(data: DataManager, selected_data: List[int]) -> dbc.Container
             dcc.Store(id=ids.TRAVAL_RESET_RULESET_BUTTON_STORE_1),
             dcc.Store(id=ids.TRAVAL_RESET_RULESET_BUTTON_STORE_2),
             dcc.Store(id=ids.TRAVAL_RESET_RULESET_BUTTON_STORE_3),
-            dcc.Store(id=ids.LOADING_QC_CHART_STORE_1),
-            dcc.Store(id=ids.LOADING_QC_CHART_STORE_2),
             dcc.Store(id=ids.QC_CHART_STORE_1),
-            dcc.Store(id=ids.QC_CHART_STORE_2),
         ],
         fluid=True,
     )
